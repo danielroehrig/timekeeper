45f8170fb5ebb463cf228b9349df9b1da706802c
Daniel Röhrig <mail@danielroehrig.de>
2024-07-16 22:35:54 +0200

    With alt screen and some color

--- timekeeper.go
+++ timekeeper.go
@@ -5,75 +5,84 @@ import (
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type model struct {
-  choices []string
-  cursor int
-  selected map[int]struct{}
+	choices  []string
+	cursor   int
+	selected map[int]struct{}
 }
 
 func initialModel() model {
-  return model{
-    choices: []string{"test 1", "test2"},
-    selected: make(map[int]struct{}),
-  }
+	return model{
+		choices:  []string{"test 1", "test2"},
+		selected: make(map[int]struct{}),
+	}
 }
 
 func (m model) Init() tea.Cmd {
- return nil 
+	return nil
 }
 
-func (m model) Update(msg tea.Msg)(tea.Model, tea.Cmd){
-  switch msg := msg.(type) {
-  case tea.KeyMsg:
-    switch msg.String() {
-    case "ctrl+c", "q":
-      return m, tea.Quit
-    case "up", "k":
-      if m.cursor > 0 {
-        m.cursor--
-      }
-    case "down", "j":
-      if m.cursor < len(m.choices)-1 {
-        m.cursor++
-      }
-  case "enter", " ":
-    _, ok := m.selected[m.cursor]
-    if ok {
-      delete(m.selected, m.cursor)
-    }else {
-      m.selected[m.cursor] = struct{}{}
-    }
-  }
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
+	return m, nil
 }
-return m, nil
-}
 
-func (m model) View() string  {
-  s := "What should\n\n"
-  for i, choice := range m.choices {
-    cursor := " "
-    if m.cursor == i {
-      cursor = ">"
-    }
-    checked := " "
-    if _, ok := m.selected[i]; ok {
-      checked = "x"
-    }
+var style = lipgloss.
+	NewStyle().
+	Bold(true).
+	Foreground(lipgloss.Color("#FAFAFA")).
+	Background(lipgloss.Color("#7D56F4")).
+	PaddingTop(2).
+	PaddingLeft(4).
+	Width(42)
 
-    s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
-  }
+func (m model) View() string {
+	s := "What should\n\n"
+	for i, choice := range m.choices {
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">"
+		}
+		checked := " "
+		if _, ok := m.selected[i]; ok {
+			checked = "x"
+		}
 
-  s += "\nPress q\n"
-  return s
+		s += style.Render(fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice))
+	}
+
+	s += "\nPress q\n"
+	return s
 }
 
 func main() {
-  p := tea.NewProgram(initialModel())
-  if _, err := p.Run(); err != nil {
-    fmt.Printf("Alas, there's been an error: %v", err)
-    os.Exit(1)
-  }
+	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
 }
-
